753b511b38862317799743ec0e1d7415a5cebc98
Hasib <hasibhsb19@gmail.com>
2026-04-27 14:45:31 +0600

    add simple validation on login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -3,9 +3,29 @@
 import React, { useState } from "react";
 import {Eye, EyeSlash} from "@gravity-ui/icons";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { authClient } from "@/lib/auth-client";
+import { error } from "better-auth/api";
 
 const LoginPage = () => {
-   const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const { register, handleSubmit, formState: { errors } }= useForm(); 
+  
+    const formHandaler = async (data) => {
+      console.log(data);
+      const { email, password } = data;
+  
+      const { data: dets, error } = await authClient.signUp.email({
+        name: name,
+        email: email,
+        password: password,
+        callbackURL: '/login',
+      });
+  
+      console.log("Data is = ",dets);
+      console.log("Error is = ", error);
+    }
 
   return (
     <div className="flex justify-center ">
@@ -16,19 +36,39 @@ const LoginPage = () => {
 
         <hr className="text-gray-200 shadow mb-5" />
 
-        <form className="px-2 space-y-2" action="#">
+        <form className="px-2 space-y-2" action="#" onSubmit={handleSubmit(formHandaler)}>
 
           {/* Email */}
           <fieldset className="fieldset">
             <legend className="fieldset-legend text-sm">Email</legend>
-            <input type="email" className="input bg-[#F3F3F3]" placeholder="Enter your email address" />
+            <input 
+              type="email" 
+              className="input bg-[#F3F3F3]" 
+              placeholder="Enter your email address"
+              {...register("email", {
+                required: "Email field is required"
+              })}
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
           </fieldset>
 
           {/* Password */}
           <fieldset className="fieldset relative">
             <legend className="fieldset-legend text-sm">Password</legend>
-            <input  type={isVisible ? "text" : "password"} className="input bg-[#F3F3F3]" placeholder="Enter your password" />
-            <button className="absolute top-4 right-7" type="button" onClick={() => setIsVisible(!isVisible)}>
+            <input 
+              type={isVisible ? "text" : "password"} 
+              className="input bg-[#F3F3F3]" 
+              placeholder="Enter your password"
+              {...register("password", {
+                required: "Password field is required"
+              })}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+            <button className="absolute top-4 right-3" type="button" onClick={() => setIsVisible(!isVisible)}>
               {isVisible ? <Eye className="size-4" /> : <EyeSlash className="size-4" />}
             </button>
           </fieldset>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -2,9 +2,28 @@
 
 import React, { useState } from "react";
 import {Eye, EyeSlash} from "@gravity-ui/icons";
+import { useForm } from "react-hook-form";
+import { authClient } from "@/lib/auth-client";
 
 const RegisterPage = () => {
    const [isVisible, setIsVisible] = useState(false);
+
+  const { register, handleSubmit } = useForm(); 
+
+  const formHandaler = async (data) => {
+    console.log(data);
+    const { name, email, password, phtotUrl } = data;
+
+    const { data: dets, error } = await authClient.signUp.email({
+      name: name,
+      email: email,
+      password: password,
+      callbackURL: '/login',
+    });
+
+    console.log("Data is = ",dets);
+    console.log("Error is = ", error);
+  }
 
   return (
     <div className="h-screen flex justify-center bg-[#F3F3F3] pb-10">
@@ -15,27 +34,47 @@ const RegisterPage = () => {
 
         <hr className="text-gray-200 shadow mb-5" />
 
-        <form className="px-2 space-y-2" action="#">
+        <form className="px-2 space-y-2" action="#" onSubmit={handleSubmit(formHandaler)} >
           {/* Name */}
           <fieldset className="fieldset">
             <legend className="fieldset-legend text-sm">Your Name</legend>
-            <input type="text" className="input bg-[#F3F3F3]" placeholder="Enter your name" />
+            <input 
+              type="text" 
+              className="input bg-[#F3F3F3]" 
+              placeholder="Enter your name"
+              {...register("name")} 
+            />
           </fieldset>
 
           <fieldset className="fieldset">
             <legend className="fieldset-legend text-sm">Photo URL</legend>
-            <input type="text" className="input bg-[#F3F3F3]" placeholder="Enter your photo url" />
+            <input 
+              type="text" 
+              className="input bg-[#F3F3F3]" 
+              placeholder="Enter your photo url"
+              {...register("phtotUrl")}
+             />
           </fieldset>
 
           <fieldset className="fieldset">
 
             <legend className="fieldset-legend text-sm">Email</legend>
-            <input type="email" className="input bg-[#F3F3F3]" placeholder="Enter your email address" />
+            <input 
+              type="email" 
+              className="input bg-[#F3F3F3]" 
+              placeholder="Enter your email address"
+              {...register("email")}
+            />
           </fieldset>
 
           <fieldset className="fieldset relative">
             <legend className="fieldset-legend text-sm">Password</legend>
-            <input  type={isVisible ? "text" : "password"} className="input bg-[#F3F3F3]" placeholder="Enter your password" />
+            <input  
+              type={isVisible ? "text" : "password"} 
+              className="input bg-[#F3F3F3]" 
+              placeholder="Enter your password"
+              {...register("password")}
+             />
             <button className="absolute top-4 right-7" type="button" onClick={() => setIsVisible(!isVisible)}>
               {isVisible ? <Eye className="size-4" /> : <EyeSlash className="size-4" />}
             </button>
